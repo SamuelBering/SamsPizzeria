8222ceacb72653e4b62f621ca94a5f2b63338f06
Samuel Bering <samuelbering@hotmail.com>
2018-01-19 16:00:36 +0100

    Completed order details with modal popup and ajax

---
+++ SamsPizzeria/Models/ViewModels/OrderDetailsModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SamsPizzeria.Models.ViewModels
+{
+    public class OrderDetailsModel
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public ICollection<DishDetailsModel> Dishes { get; set; }
+        public int Discount { get; set; }
+        public int TotalAmount { get; set; }
+    }
+}
